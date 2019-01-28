7e91b7370b4c5234b4a0200c3f331f3bd26cae69
Harald Hartmann <hh@include-once.de>
2019-01-28 16:19:03 +0100

    Lesson 238

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utillity";
 
 const initialState = {
   results: []
@@ -7,19 +8,15 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result })
-      };
+      });
     case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1);
       const newArray = state.results.filter(result => result.id !== action.id);
-      return {
-        ...state,
-        results: newArray
-      };
+      return updateObject(state, { results: newArray });
     default:
       return state;
   }
